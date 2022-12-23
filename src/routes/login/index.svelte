52875ffa0d980ae0f7f7aacfc7959d509a80d771
yinmanman <62935215+yinmanm@users.noreply.github.com>
2022-12-23 10:12:30 +0000

    code

--- src/routes/login/index.svelte
+++ src/routes/login/index.svelte
@@ -1,16 +1,35 @@
 <script>
+  import { onMount } from 'svelte';
+  import intlTelInput from 'intl-tel-input';
+  import 'intl-tel-input/build/css/intlTelInput.css';
   import { auth } from "../../firebase";
   import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 
   let phone;
   let phoneError = false;
   let phoneErrorText = "";
+  let countryCode = "";
   let phoneCode;
   let codeError = false;
   let codeErrorText = "";
   let error;
   let showGetCodeButton = true;
   let loginButtonDisabled = false;
+
+  onMount(() => {
+    const phoneInput = document.querySelector("#phone");
+    var iti = intlTelInput(phoneInput, {
+      
+    });
+    countryCode = iti.getSelectedCountryData().dialCode;
+    phoneInput.addEventListener("countrychange", function(e) {
+      // do something with iti.getSelectedCountryData()
+      countryCode = iti.getSelectedCountryData().dialCode;
+    });
+    
+  });
+
+  
 
   const generateRecaptcha = () => {
     window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
@@ -32,10 +51,11 @@
       phoneErrorText = "";
     }
     if(phone) {
+      let allPhone = "+" + countryCode + phone;
       showGetCodeButton = false;
       generateRecaptcha();
       let appVerifier = window.recaptchaVerifier;
-      signInWithPhoneNumber(auth, phone, appVerifier)
+      signInWithPhoneNumber(auth, allPhone, appVerifier)
       .then((confirmationResult) => {
         window.confirmationResult = confirmationResult;
       }).catch((error) => {
@@ -73,7 +93,6 @@
 
 <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8 font-sans">
   <div class="sm:mx-auto sm:w-full sm:max-w-md">
-    <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
     <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
   </div>
 
@@ -82,8 +101,8 @@
       <form class="space-y-6" action="#" method="POST">
         <div>
           <label for="phone" class="block text-sm font-medium text-gray-700"> Phone </label>
-          <div class="mt-1">
-            <input id="phone" name="phone" type="text" autocomplete="text" bind:value={phone} required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+          <div class="relative mt-1 rounded-md shadow-sm grid grid-cols-1">
+            <input id="phone" name="phone" type="text" autocomplete="text" bind:value={phone} required placeholder="+1 (555) 987-6543" class="block w-full rounded-md border border-gray-300 px-4 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" style="height:38px;">
           </div>
           {#if phoneError}
           <div class="mt-1 text-red-500 text-sm">{phoneErrorText}</div>
@@ -91,13 +110,13 @@
         </div>
         <div id="recaptcha-container"></div>
         <div>
-          <label for="code" class="block text-sm font-medium text-gray-700"> Phone Code </label>
+          <label for="code" class="block text-sm font-medium text-gray-700"> Verifier code </label>
           <div class="mt-1 flex rounded-md shadow-sm">
             <div class="relative flex flex-grow items-stretch focus-within:z-10">
               <input id="code" name="code" type="number" autocomplete="phone-code" bind:value={phoneCode} required class="block w-full rounded-none rounded-l-md border border-gray-300 pl-4 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" style="height:38px;">
             </div>
             <button on:click={getCode} type="button" disabled={!showGetCodeButton} class="relative -ml-px inline-flex items-center space-x-2 rounded-r-md border border-gray-300 bg-gray-50 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500">
-              <span>get code</span>
+              <span>send Code</span>
             </button>
           </div>
           {#if codeError}
