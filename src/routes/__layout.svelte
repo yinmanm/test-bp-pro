c37ca0715ed37f1faace89715f91da3c99bd192d
yinmanman <62935215+yinmanm@users.noreply.github.com>
2022-12-23 07:11:31 +0000

    layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,26 @@
 <script lang="ts">
 	import '../app.css';
+  import { onMount } from 'svelte';
+  import { onAuthStateChanged } from "firebase/auth";
+  import { auth } from "../firebase";
+
+  onMount(() => {
+    if(window.location.pathname == '/login') {
+      return
+    }else {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const uid = user.uid;
+          window.location = '/';
+        } else {
+          window.location = '/login';
+        }
+      });
+    }
+	});
+
+  
+  
 </script>
 
 <main>
