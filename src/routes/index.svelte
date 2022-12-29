3f536385dd872237be068a64965fd0a8bf2dbb01
yinmanman <62935215+yinmanm@users.noreply.github.com>
2022-12-29 06:11:49 +0000

    updated

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,8 +8,8 @@
 
 <div class="page bg-gray-50">
   <div class=""></div>
-  <nav>
-    <div class="py-3 px-6 md:py-6 md:px-10 border-b border-gray-200">
+  <nav class="relative z-20">
+    <div class="py-3 px-6 md:py-6 md:px-10">
       <div class="flex justify-between items-center">
         <div class="flex items-center space-x-2">
           <LogoText color="#101828"/>
@@ -22,8 +22,8 @@
       </div>
     </div>
   </nav>
-  <section>
-    <div class="px-6 md:px-10">
+  <section class="relative -mt-16 pt-16 md:-mt-20 md:pt-20" style="min-height:680px;">
+    <div class="px-6 md:px-10 relative z-10">
       <div class="max-w-screen-lg w-full mx-auto">
         <div class="w-full py-10 md:py-20 md:flex md:justify-between space-y-10 md:space-y-0 lg:space-x-16">
           <div class="flex-1">
@@ -62,14 +62,14 @@
                   </div>
                 </div>
                 <div data-animation-step-target="step" class="css-animation-scale hidden transform scale-0 relative mb-3">
-                  <div class="rounded-xl overflow-hidden relative z-10">
-                    <img src="/hero-animation-coffee.jpeg" class="" style="width:250px;" />
-                    <div class="p-3 pt-2 bg-white">
+                  <div class="rounded-xl shadow-t-xl relative z-10">
+                    <img src="/hero-animation-coffee.jpeg" class="rounded-t-xl" style="width:250px;" />
+                    <div class="p-3 pt-2 bg-white rounded-b-xl">
                       <div class="mb-0.5 text-xs text-gray-800">Powdur Pure Set</div>
                       <div class="text-xxs text-gray-500">https://buy.stripe.com/aF8fUK</div>
                     </div>
                   </div>
-                  <div class="absolute -right-2" style="transform: scaleX(-1);bottom:-7px;">
+                  <div class="absolute -right-2 z-10" style="transform: scaleX(-1);bottom:-7px;">
                     <MessageArrow />
                   </div>
                 </div>
@@ -82,16 +82,18 @@
         </div>
       </div>
     </div>
+    <img src="/hero-bg-color.png" class="absolute inset-x-auto inset-y-0 h-full z-0" />
   </section>
   <section class="bg-white">
     <div class="px-6 md:px-10">
       <div class="max-w-3xxl w-full mx-auto">
         <div class="py-14 md:py-32">
-          <div class="mb-14">
-            <h3 class="text-gray-900 text-3xl font-extrabold text-center">Australia’s largest network of buyers and sellers</h3>
+          <div class="mb-14 md:mb-20">
+            <h3 class="text-gray-900 text-3xl font-extrabold text-center">How it works</h3>
+            <div class="mt-4 md:mt-6 mx-auto md:text-xl text-gray-600 leading-normal text-center" style="max-width:480px;">BeachPrice Pro connects sellers with a curated list of buyers, and allows them to confidentially sell with a few simple clicks.</div>
           </div>
           <div class="">
-            <div class="mb-14 md:mb-32">
+            <div class="">
               <div class="md:flex md:justify-between md:items-center">
                 <div class="flex-1">
                   <div class="bg-gray-100 rounded-lg flex justify-evenly pt-10 pb-14">
@@ -113,7 +115,21 @@
                 </div>
               </div>
             </div>
-            <div class="mb-14 md:mb-32">
+            <div class="mt-14 md:-mt-3 md:-mb-3">
+              <div class="hidden h-40 md:flex md:justify-between md:items-center">
+                <div class="flex-1 flex justify-center h-full">
+                  <div class="flex flex-col justify-between items-center relative overflow-hidden">
+                    <div class="w-6 h-6 rounded-full bg-white border-4 border-gray-200 relative z-10"></div>
+                    <div class="flex-1 w-0 border-2 border-dashed"></div>
+                    <div class="w-6 h-6 rounded-full bg-white border-4 border-gray-200 relative z-10"></div>
+                    <div class="absolute top-0 left-2.5 css-animation-line h-10 w-0 border-2 border-indigo-500"></div>
+                  </div>
+                </div>
+                <div class="flex-1">
+                </div>
+              </div>
+            </div>
+            <div class="">
               <div class="md:flex md:justify-between md:items-center">
                 <div class="flex-1">
                   <div class="bg-gray-900 rounded-lg py-10">
@@ -129,6 +145,20 @@
                     <div class="text-gray-900 text-xl font-semibold mb-2">Platform</div>
                     <div class="text-gray-600">We source the best opportunities from our network of private owners and quota brokers.</div>
                   </div>
+                </div>
+              </div>
+            </div>
+            <div class="mt-14 md:-mt-3 md:-mb-3">
+              <div class="hidden h-40 md:flex md:justify-between md:items-center">
+                <div class="flex-1 flex justify-center h-full">
+                  <div class="flex flex-col justify-between items-center relative overflow-hidden">
+                    <div class="w-6 h-6 rounded-full bg-white border-4 border-gray-200 relative z-10"></div>
+                    <div class="flex-1 w-0 border-2 border-dashed"></div>
+                    <div class="w-6 h-6 rounded-full bg-white border-4 border-gray-200 relative z-10"></div>
+                    <div class="absolute top-0 left-2.5 css-animation-line2 h-10 w-0 border-2 border-indigo-500"></div>
+                  </div>
+                </div>
+                <div class="flex-1">
                 </div>
               </div>
             </div>
@@ -163,19 +193,19 @@
       </div>
     </div>
   </section>
-  <section>
+  <section class="bg-gray-900">
     <div class="px-6 md:px-10">
       <div class="max-w-screen-lg w-full mx-auto">
         <div class="pt-14 pb-6 md:pt-24 md:pb-20">
           <div class="w-full md:flex md:justify-between md:items-center">
             <div class="md:max-w-base w-full">
-              <div class="uppercase text-xs font-medium tracking-wide-1em" style="color:#004EEB;">Why sell with us</div>
-              <div class="mt-5 text-gray-900 text-3xl leading-tight font-extrabold">Sell quickly, easily, and for the highest price</div>
-              <div class="mt-6 text-gray-600 text-xl leading-normal">List your quota or entitlements for sale and instantly reach thousands of verified buyers. It’s fast, free, and anonymous.</div>
+              <div class="uppercase text-gray-400 text-xs font-medium tracking-wide-1em" style="">For Sellers</div>
+              <div class="mt-5 text-white text-3xl leading-tight font-extrabold">Sell quickly, easily, and for the highest price</div>
+              <div class="mt-6 text-gray-300 text-xl leading-normal">List your quota or entitlements for sale and instantly reach thousands of verified buyers. It’s fast, free, and anonymous.</div>
               <ul class="mt-8 flex flex-col space-y-6">
-                <li class="text-gray-600 leading-tight">Run a competitive sales campaign in minutes</li>
-                <li class="text-gray-600 leading-tight">100% seller protection with BeachPrice Escrow</li>
-                <li class="text-gray-600 leading-tight">No percentage based comissions</li>
+                <li class="text-gray-300 leading-tight">Run a competitive sales campaign in minutes</li>
+                <li class="text-gray-300 leading-tight">100% seller protection with BeachPrice Escrow</li>
+                <li class="text-gray-300 leading-tight">No percentage based comissions</li>
               </ul>
             </div>
             <div class="">
@@ -192,7 +222,7 @@
         <div class="pt-14 pb-6 md:pt-24 md:pb-20">
           <div class="w-full md:flex md:justify-between md:items-center">
             <div class="md:max-w-base w-full">
-              <div class="uppercase text-green-600 text-xs font-medium tracking-wide-1em" style="">why buy through us</div>
+              <div class="uppercase text-gray-500 text-xs font-medium tracking-wide-1em" style="">For Buyers</div>
               <div class="mt-5 text-gray-900 text-3xl leading-tight font-extrabold">Access and purchase the very best quota opportunities</div>
               <div class="mt-6 text-gray-600 text-xl leading-normal">By joining BeachPrice Pro, you’ll be on the exclusive waitlist for new opportunities as they arise. You’ll also get access to all current opportunities.</div>
               <ul class="mt-8 flex flex-col space-y-6">
@@ -230,6 +260,62 @@
   }
   .css-animation-scale {
     transition: all 300ms ease-in-out;
+  }
+  .css-animation-line {
+    animation:lineThrough 5.5s linear infinite;
+	  -webkit-animation:lineThrough 5.5s linear infinite;
+  }
+  .css-animation-line2 {
+    animation:lineThrough2 5.5s linear infinite;
+	  -webkit-animation:lineThrough2 5.5s linear infinite;
+  }
+  @keyframes lineThrough
+  {
+    0% {
+      top: -40px;
+    }
+    50% {
+      top: 140px;
+    }
+    100% {
+      top: 140px;
+    }
+  }
+  @-webkit-keyframes lineThrough
+  {
+    0% {
+      top: -40px;
+    }
+    50% {
+      top: 140px;
+    }
+    100% {
+      top: 140px;
+    }
+  }
+  @keyframes lineThrough2
+  {
+    0% {
+      top: -40px;
+    }
+    50% {
+      top: -40px;
+    }
+    100% {
+      top: 140px;
+    }
+  }
+  @-webkit-keyframes lineThrough2
+  {
+    0% {
+      top: -40px;
+    }
+    50% {
+      top: -40px;
+    }
+    100% {
+      top: 140px;
+    }
   }
 
 /*   .step-show-1 {
